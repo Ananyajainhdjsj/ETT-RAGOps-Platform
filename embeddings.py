14a05ee4daf232639f8d7031e40521b3fd6dd0b1
Ananya Jain <ananya.23fe10cse00086@muj.manipal.edu>
2026-05-10 13:06:39 +0530

    Stop tracking files

--- embeddings.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-embed_model = SentenceTransformer("all-MiniLM-L6-v2")
-
-def get_embedding(text: str):
-    return embed_model.encode([text])[0]
